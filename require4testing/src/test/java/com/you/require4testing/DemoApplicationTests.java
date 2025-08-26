213bac8528f1bce3e7ef5cf46e67712c9ee01f0d
JinyuanZhang <jinyuan.zhang@stud.tu-darmstadt.de>
2025-08-26 20:12:51 +0200

    catch spring

---
+++ require4testing/src/test/java/com/you/require4testing/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.you.require4testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
